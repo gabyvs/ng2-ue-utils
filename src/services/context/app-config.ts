fb8878b587bbcc67ab9861a3543452a8b667199f
michaelyara <michaelyara@google.com>
2017-05-01 22:37:19 -0600

    Added GTM error category to IAppConfig.

--- src/services/context/app-config.ts
+++ src/services/context/app-config.ts
@@ -5,6 +5,7 @@ export interface IAppConfig {
     appBasePath: string;
     appVersion?: string;
     gtmAppName: string;
+    gtmErrorCategory?: string;
 }
 
 export const APP_CONFIG = new OpaqueToken('AppConfig');
