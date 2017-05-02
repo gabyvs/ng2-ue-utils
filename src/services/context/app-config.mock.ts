d7d252ef4cc6560d1f3142b973ddfb6bf67f49b5
michaelyara <michaelyara@google.com>
2017-05-02 14:15:47 -0600

    Added gtmErrorCategory value to mockAppConfig.

--- src/services/context/app-config.mock.ts
+++ src/services/context/app-config.mock.ts
@@ -4,9 +4,11 @@ const apiBasePath = 'apiproducts';
 const appBasePath = 'products';
 const gtmAppName = 'ProductsSPA';
 const version = '2017.04.12.0';
+const gtmErrorCategory = 'productErrorCategory';
 export const mockAppConfig: IAppConfig = {
     apiBasePath: apiBasePath,
     appBasePath: appBasePath,
     appVersion: version,
-    gtmAppName: gtmAppName
+    gtmAppName: gtmAppName,
+    gtmErrorCategory: gtmErrorCategory
 };
