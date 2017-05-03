d0724f756000592837d2908ba419614d4e02fd98
michaelyara <michaelyara@google.com>
2017-05-02 21:22:11 -0600

    Modified GTMErrorHandler unit test to use wrapped error.

--- src/services/context/gtm-error-handler.spec.ts
+++ src/services/context/gtm-error-handler.spec.ts
@@ -6,7 +6,11 @@ import {ErrorHandler} from '@angular/core';
 import {GTMErrorHandler} from './gtm-error-handler';
 
 const testErrorMessage: string = 'test error message';
-const testError: Error = new Error(testErrorMessage);
+const testWrappedError = {
+    rejection: {
+        originalError: new Error(testErrorMessage)
+    }
+};
 const mockAppConfig: IAppConfig = {
     apiBasePath: 'test base path',
     appBasePath: 'test base path',
@@ -15,24 +19,24 @@ const mockAppConfig: IAppConfig = {
     gtmErrorCategory: 'test error category'
 };
 const expectedEvent: IGAEventProps = {
-    action: 'Error: ' + testErrorMessage,
+    action: testErrorMessage,
     category: mockAppConfig.gtmErrorCategory,
     label: 'version: ' + mockAppConfig.appVersion,
     noninteraction: true,
     value: 0
 };
+const consoleErrorFunc = console.error; // Backup error function for restoration in afterEach()
 
-let consoleErrorFunc = console.error; // Backup error function for restoration in afterEach()
 let errorHandler: GTMErrorHandler;
 let gtmService: GTMServiceMock;
 
 /**
  * Configures the TestBed with a specific instance of IAppConfig.
  *
- * Normally this would be done in beforeEach() but we need to inject different variations of IAppConfig for each test.
+ * Normally this would be done in beforeEach(), but we need to inject different variations of IAppConfig for each test.
  * Injecting the object and modifying it after the fact does not seem to work.
  *
- * @param appConfig the IAppConfig to inject into the testbed
+ * @param appConfig the IAppConfig to inject into the TestBed
  */
 function configureTestBed(appConfig: IAppConfig) {
     TestBed.configureTestingModule({
@@ -52,7 +56,7 @@ function configureTestBed(appConfig: IAppConfig) {
  */
 function executeErrorHandler() {
     try {
-        errorHandler.handleError(testError);
+        errorHandler.handleError(testWrappedError);
     } catch (e) {
         // Expected
     }
